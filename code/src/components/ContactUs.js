b0b7842adc3111e41e2856f40964ac7294b428e3
MarySnopok <marysnopok8@gmail.com>
2021-11-04 15:42:07 +0100

    file addition

--- code/src/components/ContactUs.js
+++ code/src/components/ContactUs.js
@@ -5,13 +5,12 @@ import React from "react";
 export const ContactUs = () => {
   return (
     <div className="contact-us">
-      Developed by
-      <a className="github-link" href="https://github.com/madeleinesvensson" target="_blank" rel="noopener noreferrer">
-        <span>Madeleine Svensson</span>
-      </a>
-      <span>&</span>
       <a className="github-link" href="https://github.com/MarySnopok" target="_blank" rel="noopener noreferrer">
         <span>Mary Snopok</span>
+      </a>
+      <span>&</span>
+      <a className="github-link" href="https://github.com/madeleinesvensson" target="_blank" rel="noopener noreferrer">
+        <span>Madeleine Svensson</span>
       </a>
     </div>
   );
